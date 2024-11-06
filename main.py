752139abf5d8a15ecdc9ca32605b89bb769a1bf0
Suhani Bhanvadia <suhanihb@gmail.com>
2024-11-06 11:00:38 -0800

    Created parseManifest() and a sample manifest.

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 
 def main():
-    print("HIIII")
-    
-def parseMaifest():
-    with open('manifest.txt', 'r') as file:
-    content = file.read()
-    # insert code
+    manifest_data = parseManifest("sampleManifest.txt")
+    for container in manifest_data:
+        print(container)
+        
+def parseManifest(filePath):
+    manifest = []
 
-def updateMaifest():
-    # insert code
+    with open(filePath, 'r') as file:
+        # update log with "Manifest name.txt is opened"
+        for line in file:
+            line = line.strip()
+            locationPart, weightPart, descriptionPart = line.split(", ")
+            location = tuple(map(int, locationPart.strip("[]").split(',')))
+            weight = int(weightPart.strip("{}"))
+            description = descriptionPart
+            manifest.append([location, weight, description])
 
-def load():
-    # insert code   
+    return manifest
 
-def unload():
-    # insert code
+# def updateMaifest():
+#     # insert code
 
-def balance():
-    # insert code
+# def load():
+#     # insert code   
 
-def updateLog():
-    # insert code
+# def unload():
+#     # insert code
 
-def getMoves():
-    # insert code
+# def balance():
+#     # insert code
+
+# def updateLog():
+#     # insert code
+
+# def getMoves():
+#     # insert code
 
 if __name__=="__main__":
     main()
