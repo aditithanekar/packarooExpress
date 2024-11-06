245c741e49516201961f8e9ae0722f52735440e9
Suhani Bhanvadia <suhanihb@gmail.com>
2024-11-06 15:38:27 -0800

    Added updateLog() with timestamps.

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from datetime import datetime
 import pytz
+import requests
 from container import container
 
 def main():
-    # updateLog()
+    updateLog("Insert Message to log.txt")
+    
     manifestData = parseManifest("sampleManifest.txt")
     for container in manifestData:
         container.printContainer()
         
+# To store each container object from manifest.txt into a list
 def parseManifest(filePath):
-    # stores container objects
     manifest = []
 
     with open(filePath, 'r') as file:
-        # update log with "Manifest name.txt is opened"
+        updateLog("Manifest {} is opened".format(filePath))
         for line in file:
             line = line.strip()
             locationPart, weightPart, descriptionPart = line.split(", ")
@@ -39,12 +41,37 @@ def parseManifest(filePath):
 # def balance():
 #     # insert code
 
-def updateLog():
-    pstTimezone = pytz.timezone('America/Los_Angeles')
-    pstTime = datetime.now(pstTimezone)
-    pstTimeFormatted = pstTime.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+# To get current PST time from an external source, for updateLog()
+def getCurrentTime():
+    try:
+        response = requests.get("http://worldtimeapi.org/api/timezone/America/Los_Angeles")
+        response.raise_for_status()  # Raises an exception if there is an error in the request
+        data = response.json()
+        datetimeString = data["datetime"]
+        utcTime = datetime.strptime(datetimeString, "%Y-%m-%dT%H:%M:%S.%f%z")
+        pstTimezone = pytz.timezone('America/Los_Angeles')
+        pstTime = utcTime.astimezone(pstTimezone)
+        return pstTime
+    except requests.RequestException as e:
+        print("Error fetching time:", e)
+        return None
 
-    print(pstTimeFormatted)
+# To update log file with a message string
+# USAGE: updateLog("Insert Message to log.txt")
+def updateLog(message):
+    pstTime = getCurrentTime()
+
+    if 10<=pstTime.day % 100<=20: suffix = "th"
+    else: suffix = {1:"st", 2:"nd", 3:"rd"}.get(pstTime.day % 10, "th")
+    
+    month_day_year = pstTime.strftime(f"%B {pstTime.day}{suffix} %Y")
+    timePart = pstTime.strftime("%H:%M")
+    pstTimeFormatted = f"{month_day_year}: {timePart}"
+    logInput = f"{pstTimeFormatted} {message}"
+    print(logInput)
+    
+    with open("log.txt", "a") as logFile:
+        logFile.write(logInput + "\n")
 
 # def getMoves():
 #     # insert code
