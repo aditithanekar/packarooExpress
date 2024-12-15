1d634d38efa125cfdf13337f6e4856b41d3385e6
Suhani Bhanvadia <suhanihb@gmail.com>
2024-12-14 21:34:14 -0800

    fix load/unload time, add unload trace

--- test.py
+++ test.py
@@ -1,91 +1,40 @@
 import main
 from state import State
 from container import Container
-from utils import parseManifest
-from main import load
-from balance2 import a_star
 import utils
 from load_unload import load
 import load_unload
 
-MANIFEST = utils.parseManifest("ShipCase1.txt")
+##### TIME AND TRACE IMPLEMENTATION #######################################################################
+
+MANIFEST = utils.parseManifest("test_cases/ShipCase3.txt")
 
 state = State()
-state.init_start_state(MANIFEST)
-
-
-# ADITI TESTING STUFF ===============================================================================================
-def adititest():
-    state.print_state_representation()
-    
-    result_state = a_star(state)
-    print(result_state)
-    #result_state.print_state_representation()
-
-adititest()
-
-# SUHANI TESTING STUFF ===============================================================================================
-# def suhani_test():
-#     updated_state.print_state_representation()
-    
-#     containers = [Container(position=None, weight=10.0 + i, description=f"Container {i}") for i in range(1)]
-#     for container in containers:
-#         print(container.print_node_description())
-        
-#     result_state = load(updated_state, containers)
-#     result_state.print_state_representation()
-
-# suhani_test()
-
-# load(updated_state, MANIFEST)
-
 state = state.init_start_state(MANIFEST)
 state.print_state_representation()
 
-containers = [Container(position=None, weight=10.0 + i, description=f"Box{i}") for i in range(11)]
+# UNLOADING
+target = Container([1,0], 99, "Cow")
+unload_targets = [target]
+unloaded_state, trace, blocking_containers = load_unload.unload(state, unload_targets, (8,0))
+    # unload() now returns a state, a trace without blocking containers, and a list of blocking containers
+use_this_trace, time = load_unload.unload_time_trace(unloaded_state, trace, blocking_containers)
+    # unload_time_trace() returns a total time and a list of pairs of coordinates showing old&new location of each container moved (unloaded containers AND moving blocking containers)
+print(use_this_trace)
+print("Total unload time = ", time)
 
-loaded_state, operations = load(state, containers) # operations is for if printing steps is needed later
+# LOADING
+containers = [Container(position=None, weight=10.0 + i, description=f"Box{i}") for i in range(2)]
+loaded_state, operations = load(unloaded_state, containers)
+print(operations) # operations is a list of coordinates where each container was loaded (IGNORE THE FIRST ONE, ITS JUST 8,0)
+
 loaded_state.print_state_representation()
+print(loaded_state.time)
 
-utils.updateMaifest(loaded_state, "updatedManifest.txt")
+# utils.updateMaifest(loaded_state, "updatedManifest.txt")
 
-# #USING MOCK MANIFEST------------------------------------------------------------------------------------------------------------
-# def test_load():
-    
-#     mock_manifest = [
-#         Container([1, 0], 10, 'UNUSED'),
-#         Container([2, 0], 15, 'UNUSED'),
-#         Container([3, 0], 20, 'USED')
-#     ]
-    
-    
-#     global MANIFEST
-#     original_manifest = MANIFEST
-#     MANIFEST = mock_manifest
-    
-    
-#     start_state = State()
-#     start_state.init_start_state()
-    
-    
-#     load_list = [c for c in mock_manifest if c.get_description() == 'UNUSED']
-    
-    
-#     result_state = load(start_state, load_list)
-    
-    
-#     assert result_state is not None, "Load function should return a state"
-#     assert len(load_list) == 0, "All containers should be loaded"
-    
-    
-#     result_state.print_state_representation()
-    
-    
-#     MANIFEST = original_manifest
-
-#     print("Load test completed successfully!")
-
-
-# test_load()
-
-
+# BALANCE (TIME)
+balance_input = [((0, 3), (0, 4), 1), ((0, 4), (0, 5), 1), ((0, 2), (1, 2), 1), ((0, 1), (0, 2), 1), ((0, 2), (0, 3), 1), ((0, 3), (0, 4), 1), ((0, 4), (1, 4), 1), ((1, 4), (1, 5), 1), ((1, 5), (1, 6), 1)]
+def get_balance_time(balance_input):
+    time += balance_input[0]
+#################################################################################################################################
